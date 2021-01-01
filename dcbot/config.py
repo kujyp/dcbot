6c295a99b85f62ffe66efb7ed620ef440e2de651
kujyp <swe.jaeyoungpark@navercorp.com>
2021-01-02 01:14:36 +0900

    Implement ensure_signin

---
+++ dcbot/config.py
@@ -0,0 +1,22 @@
+import os
+
+# Ref: https://www.selenium.dev/selenium/docs/api/py/webdriver_remote/selenium.webdriver.remote.webdriver.html?highlight=implicitly#selenium.webdriver.remote.webdriver.WebDriver.implicitly_wait
+SELENIUM_TIME_TO_WAIT_SECONDS = 5
+
+# Ref: https://chromedriver.storage.googleapis.com/index.html
+CHROME_DRIVER_VERSION = "87.0.4280.88"
+CHROME_DRIVER_FILENAME_LINUX = "chromedriver_linux64.zip"
+CHROME_DRIVER_FILENAME_MAC = "chromedriver_mac64.zip"
+CHROME_DRIVER_FILENAME_WINDOWS = "chromedriver_win32.zip"
+CHROME_DRIVER_ZIPCONTENT_FILENAME = "chromedriver"
+
+CHROME_DRIVER_URL_ROOT = "https://chromedriver.storage.googleapis.com"
+CHROME_DRIVER_URL_LINUX = f"{CHROME_DRIVER_URL_ROOT}/{CHROME_DRIVER_VERSION}/{CHROME_DRIVER_FILENAME_LINUX}"
+CHROME_DRIVER_URL_MAC = f"{CHROME_DRIVER_URL_ROOT}/{CHROME_DRIVER_VERSION}/{CHROME_DRIVER_FILENAME_MAC}"
+CHROME_DRIVER_URL_WINDOWS = f"{CHROME_DRIVER_URL_ROOT}/{CHROME_DRIVER_VERSION}/{CHROME_DRIVER_FILENAME_WINDOWS}"
+
+DCBOT_PACKAGE_DIRPATH = os.path.dirname(__file__)
+CHROME_DRIVER_INSTALL_DIRPATH = os.path.join(DCBOT_PACKAGE_DIRPATH, "driver")
+CHROME_DRIVER_INSTALL_PATH = os.path.join(
+    CHROME_DRIVER_INSTALL_DIRPATH, f"{CHROME_DRIVER_ZIPCONTENT_FILENAME}_{CHROME_DRIVER_VERSION}"
+)
--- dcbot/dc.py
+++ dcbot/dc.py
@@ -1,3 +1,125 @@
+import os
+import shutil
+import stat
+from sys import platform
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from dcbot.config import DCBOT_PACKAGE_DIRPATH, \
+    CHROME_DRIVER_ZIPCONTENT_FILENAME, CHROME_DRIVER_INSTALL_DIRPATH, \
+    CHROME_DRIVER_INSTALL_PATH, CHROME_DRIVER_URL_LINUX, CHROME_DRIVER_URL_MAC, CHROME_DRIVER_URL_WINDOWS, \
+    SELENIUM_TIME_TO_WAIT_SECONDS
+
+
+def ensure_driver_installed():
+    if os.path.exists(CHROME_DRIVER_INSTALL_PATH):
+        if os.path.isfile(CHROME_DRIVER_INSTALL_PATH) and os.access(CHROME_DRIVER_INSTALL_PATH, os.X_OK):
+            return
+        else:
+            shutil.rmtree(CHROME_DRIVER_INSTALL_DIRPATH)
+
+    # Ref: https://docs.python.org/3/library/sys.html#sys.platform
+    if platform.startswith("linux"):
+        url = CHROME_DRIVER_URL_LINUX
+    elif platform == "darwin":
+        url = CHROME_DRIVER_URL_MAC
+    elif platform == "win32" or platform == "cygwin":
+        url = CHROME_DRIVER_URL_WINDOWS
+    else:
+        raise EnvironmentError(f"Not supported OS. [{platform}]")
+
+    filename = url.split("/")[-1]
+
+    driver_dirpath = os.path.join(DCBOT_PACKAGE_DIRPATH, "driver")
+    download_path = os.path.join(driver_dirpath, filename)
+    if not os.path.isdir(driver_dirpath):
+        if os.path.isfile(driver_dirpath):
+            print(f"Remove file [{driver_dirpath}]...")
+            os.remove(driver_dirpath)
+        os.makedirs(driver_dirpath)
+    print(f"Download driver [{url}]...")
+    _filename, _headers = urlretrieve(url, download_path)
+    with ZipFile(download_path, 'r') as zf:
+        assert CHROME_DRIVER_ZIPCONTENT_FILENAME in zf.namelist(), \
+            f"[{url}] doesn't contain '{CHROME_DRIVER_ZIPCONTENT_FILENAME}' file."
+        print(f"Install driver on [{CHROME_DRIVER_INSTALL_DIRPATH}]...")
+        zf.extract(CHROME_DRIVER_ZIPCONTENT_FILENAME, CHROME_DRIVER_INSTALL_DIRPATH)
+        shutil.move(
+            os.path.join(CHROME_DRIVER_INSTALL_DIRPATH, CHROME_DRIVER_ZIPCONTENT_FILENAME),
+            CHROME_DRIVER_INSTALL_PATH,
+        )
+
+    os.remove(download_path)
+    os.chmod(CHROME_DRIVER_INSTALL_PATH, os.stat(CHROME_DRIVER_INSTALL_PATH).st_mode | stat.S_IEXEC)
+
+
+class WebDriverContainer:
+    def __init__(self) -> None:
+        ensure_driver_installed()
+        print(f"Opening web browser...")
+        self.driver = WebDriver(CHROME_DRIVER_INSTALL_PATH)
+        self.driver.implicitly_wait(SELENIUM_TIME_TO_WAIT_SECONDS)
+
+    def __del__(self):
+        print(f"Closing web browser...")
+        self.driver.close()
+
+    def get(self, url: str, print_log: bool = True):
+        if print_log:
+            print(f"Retrieve url [{url}]...")
+        self.driver.get(url)
+
+    def find_element_by_id(self, id_: str):
+        WebDriverWait(self.driver, SELENIUM_TIME_TO_WAIT_SECONDS) \
+            .until(expected_conditions.presence_of_element_located((By.ID, id_)))
+        return self.driver.find_element_by_id(id_)
+
+    def find_element_by_class_name(self, name: str):
+        WebDriverWait(self.driver, SELENIUM_TIME_TO_WAIT_SECONDS) \
+            .until(expected_conditions.presence_of_element_located((By.CLASS_NAME, name)))
+        return self.driver.find_element_by_class_name(name)
+
+    @property
+    def current_url(self):
+        return self.driver.current_url
+
+
+class DcBrowser:
+    def __init__(self, web_driver_container: WebDriverContainer, dc_account: str, dc_password: str) -> None:
+        self.web_driver_container = web_driver_container
+        self.dc_account = dc_account
+        self.dc_password = dc_password
+
+    def ensure_signin(self):
+        if self.is_signed():
+            return
+        self.web_driver_container.get("https://m.dcinside.com/auth/login")
+        element_userid = self.web_driver_container.find_element_by_id("user_id")
+        element_userid.send_keys(self.dc_account)
+        element_userpw = self.web_driver_container.find_element_by_id("user_pw")
+        element_userpw.send_keys(self.dc_password)
+        element_loginok = self.web_driver_container.find_element_by_id("login_ok")
+        print("Signing in...")
+        before_signin_url = self.web_driver_container.current_url
+        element_loginok.click()
+
+        WebDriverWait(self.web_driver_container.driver, SELENIUM_TIME_TO_WAIT_SECONDS) \
+            .until(lambda driver: driver.current_url != before_signin_url)
+
+    def is_signed(self):
+        self.web_driver_container.get("https://m.dcinside.com/aside")
+        element_loginbox = self.web_driver_container.find_element_by_class_name("login-box")
+        return "로그인 해주세요." not in element_loginbox.text
+
+
 def post(dc_account: str, dc_password: str, gall_id: str, title: str, content: str):
-    print(dc_account)
-    print(gall_id)
+    web_driver_container = WebDriverContainer()
+    dc_browser = DcBrowser(web_driver_container, dc_account, dc_password)
+    dc_browser.ensure_signin()
+    import time
+    time.sleep(5)
